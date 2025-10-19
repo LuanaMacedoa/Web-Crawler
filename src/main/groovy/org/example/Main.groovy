99060a3e0ac756301c64ac8bd54da541fe4c31f6
luana <luanagabriellapsm@gmail.com>
2025-10-19 12:31:04 -0300

    implementação task2

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -16,6 +16,8 @@ class Main {
 
         def paginaInicialDoc = Jsoup.connect(url).get()
         def linksPaginaInicial = paginaInicialDoc.select("a[href]")
+        def padraoTisdoc = ''
+
 
         Element espacoPrestadorLink = linksPaginaInicial.find { it.text().contains("Espaço do Prestador") }
 
@@ -30,7 +32,9 @@ class Main {
             String urlPadraoTrocaInformacao = link.attr("abs:href")
             if (urlEspacoPrestador != null) {
                 println("link2 encontrado: $urlPadraoTrocaInformacao")
-                def padraoTisdoc = Jsoup.connect(urlPadraoTrocaInformacao).get()
+                padraoTisdoc = Jsoup.connect(urlPadraoTrocaInformacao).get()
+
+
                 def linksPadraoTis = padraoTisdoc.select("a[href]")
                 Element link3 = linksPadraoTis.find { it.text().contains("Clique aqui para acessar a versão Setembro/2025") }
                 String urlPadraotis = link3.attr("abs:href")
@@ -71,13 +75,72 @@ class Main {
                 }
 
 
-
-            } else{
+            } else {
                 println "Link2 não encontrado"
             }
 
         } else {
             println("Link não encontrado.")
+        }
+
+        // implementação parte 2
+        def historicoTiss = padraoTisdoc.select("a[href]")
+        Element linkHistorico = historicoTiss.find { it.text().contains("Clique aqui para acessar todas as versões dos Componentes") }
+
+        String linkAbsHistorico = ''
+        if(linkHistorico != null){
+            linkAbsHistorico = linkHistorico.attr('abs:href')
+            println("link 4: $linkAbsHistorico")
+        }else {
+            println("Link 4 não encontrado")
+        }
+
+        Document historicoTisDoc = Jsoup.connect(linkAbsHistorico).get()
+        Element tabela = historicoTisDoc.select("div#parent-fieldname-text table").first()
+        Element cabecalhoTr = tabela.select("thead tr").first()
+
+        def colunas = cabecalhoTr.select("th").take(3)
+        def linhasDados = tabela.select("tbody tr")
+        def linhasInvertidas = linhasDados.reverse()
+
+        List<String> LinhaCsv = []
+
+        def cabecalhoCsv = colunas.collect {it.text().trim()}.join(",")
+        LinhaCsv.add(cabecalhoCsv)
+        println(LinhaCsv)
+
+        /*
+            colunas.each{th ->
+                print(th.text() + " ")
+            }
+            println(" ")
+        */
+        Boolean coletar = false
+
+        for (def linha: linhasInvertidas) {
+            def coluna = linha.select("td")
+            if (coluna.size() >= 3) {
+                def competencia = coluna[0].text().trim()
+                if (competencia == "Jan/2016"){
+                    coletar = true
+                }
+                if (coletar) {
+                    def publicacao = coluna[1].text().trim()
+                    def inicioVigencia = coluna[2].text().trim()
+
+                    LinhaCsv.add([competencia,publicacao,inicioVigencia].join(","))
+                }
+            }
+        }
+        def pastaDestino = "./DownloadsTiss"
+        def nomeArq = "HistoricoVersoes.csv"
+        def caminhoArquivo = Paths.get(pastaDestino,nomeArq)
+
+        try {
+            Files.write(caminhoArquivo, LinhaCsv, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+            println("Arquivo salvo em: $caminhoArquivo")
+        } catch (Exception e) {
+            println("Erro ao salvar arquivo: ${e.message}")
         }
 
 
